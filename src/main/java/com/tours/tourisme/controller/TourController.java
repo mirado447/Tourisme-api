8ad35b0b91e469dce1b41b7ab5007ee76db30030
Mirado <hei.mirado@gmail.com>
2024-06-20 09:07:01 +0300

    feat: add getAllTour controller

---
+++ src/main/java/com/tours/tourisme/controller/TourController.java
@@ -0,0 +1,25 @@
+package com.tours.tourisme.controller;
+
+import com.tours.tourisme.model.BoundedPageSize;
+import com.tours.tourisme.model.PageFromOne;
+import com.tours.tourisme.repository.entity.Tour;
+import com.tours.tourisme.service.TourService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class TourController {
+    private final TourService tourService;
+    @GetMapping("/tours")
+    public List<Tour> getAllTour(@RequestParam int page,
+                                 @RequestParam(value = "page_size") int pageSize){
+        PageFromOne pageFromOne = new PageFromOne(page);
+        BoundedPageSize boundedPageSize = new BoundedPageSize(pageSize);
+        return tourService.getAllTour(pageFromOne, boundedPageSize);
+    }
+}
---
+++ src/main/java/com/tours/tourisme/service/TourService.java
@@ -0,0 +1,23 @@
+package com.tours.tourisme.service;
+
+import com.tours.tourisme.model.BoundedPageSize;
+import com.tours.tourisme.model.PageFromOne;
+import com.tours.tourisme.repository.entity.Tour;
+import com.tours.tourisme.repository.TourRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class TourService {
+    private final TourRepository repository;
+
+    public List<Tour> getAllTour(PageFromOne page, BoundedPageSize pageSize){
+        Pageable pageable = PageRequest.of(page.getValue() - 1, pageSize.getValue());
+        return repository.findAllTour(pageable);
+    }
+}
