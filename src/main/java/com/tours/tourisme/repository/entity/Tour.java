0640167499a3f0dfa9f90d904f276212f90d0eb5
Mirado <hei.mirado@gmail.com>
2024-06-21 23:34:54 +0300

    add name atribut for tour

--- src/main/java/com/tours/tourisme/repository/entity/Tour.java
+++ src/main/java/com/tours/tourisme/repository/entity/Tour.java
@@ -23,6 +23,8 @@ public class Tour {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
